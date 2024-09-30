a338cc72dc8440ab02e59450358538248c88c8ec
hinoob <reio.poldmaa@voco.ee>
2024-09-30 09:03:30 +0300

    Added handlebars template engine

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require("express");
 const app = express()
 
 const path = require("path")
+const hbs = require("express-handlebars");
+
+app.set("views", path.join(__dirname, 'views'));
+app.set("view engine", "hbs");
+app.engine('hbs', hbs.engine({
+    extname: "hbs",
+    defaultLayout: "main",
+    layoutsDir: __dirname + "/views/layouts"
+}))
+
 const mysql = require("mysql2")
 
 const bodyparser = require("body-parser")
